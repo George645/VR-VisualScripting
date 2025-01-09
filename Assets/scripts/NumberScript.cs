0ecf35b28a196ed88ac39fcdd97584ee41a585b7
George645 <gsvbutterfly@outlook.com>
2025-01-09 16:15:47 +0000

    Numbers and started scripts

---
+++ Assets/scripts/NumberScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NumberScript : MonoBehaviour
+{
+    NumberScript rightThing;
+    bool isNumber = true;
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/OperationsHub.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class OperationsHub : MonoBehaviour{
+    public static int Addition(NumberScript input) {
+        if (input.rightThing.isNumber) {
+            Addition()
+        }
+    }
+    public static int GetEntireNumber(NumberScript input) {
+        int returnint = input;
+        if (input.rightThing.isnumber) {
+
+        }
+    }
+}
