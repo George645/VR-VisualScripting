68b59d2d3255a301322e54d2af5cd050719178ca
George645 <gsvbutterfly@outlook.com>
2025-01-13 16:16:12 +0000

    selection and start of duplication
    Do more on duplication

--- Assets/scripts/OperationsHub.cs
+++ Assets/scripts/OperationsHub.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
+using Unity.VisualScripting;
 
+[IncludeInSettings(true)]
 public class OperationsHub : MonoBehaviour{
     public static int Addition(CharacterScript input1, CharacterScript input2) { //the added plus will be the thing to run this function
         int firstInt = GetEntireNumber(input1);
