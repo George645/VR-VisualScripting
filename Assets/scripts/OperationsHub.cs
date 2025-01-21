baec3ffa0aef5cbf928570b7e0b693f7a2918717
George645 <gsvbutterfly@outlook.com>
2025-01-21 14:48:35 +0000

    asd

--- Assets/scripts/OperationsHub.cs
+++ Assets/scripts/OperationsHub.cs
@@ -4,23 +4,26 @@ using Unity.VisualScripting;
 
 [IncludeInSettings(true)]
 public class OperationsHub : MonoBehaviour{
-    public static int Addition(CharacterScript input1, CharacterScript input2) { //the added plus will be the thing to run this function
+    /*public static int Addition(CharacterScript input1, CharacterScript input2) { //the added plus will be the thing to run this function
         int firstInt = GetEntireNumber(input1);
         int secondInt = GetEntireNumber(input2);
         return firstInt + secondInt;
-    }
-    public static int GetEntireNumber(CharacterScript input) {
-        if (input.leftThing != null && input.leftThing.isNumber) {
-            int returnInt = GetEntireNumber(input.leftThing);
+    }*/
+    public static int GetEntireNumberFromLeft(GameObject input) {
+        GameObject leftThing = (GameObject)Variables.Object(input).Get("leftthing");
+        if (leftThing != null && (bool)Variables.Object(leftThing).Get("isnumber")) {
+            int returnInt = GetEntireNumberFromLeft(leftThing) + (int)Variables.Object(input).Get("character");
             return returnInt;
         }
-        string returningNumber = Convert.ToString(input.character);
+        else {
+            return (int)Variables.Object(input).Get("character");
+        }
+    }
+    public static void GetEntireNumberFromRight(GameObject input) {
+        /*string returningNumber = Convert.ToString(input.character);
         if (input.rightThing != null && input.rightThing.isNumber) {
             returningNumber += Convert.ToString(GetEntireNumber(input.rightThing));
             return Convert.ToInt32(returningNumber);
-        }
-        else {
-            return Convert.ToInt32(returningNumber);
-        }
+        }*/
     }
 }
